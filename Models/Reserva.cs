2a1075253595a7de6ace27be7c55edb759ba4199
Victor prado <victorpradorm@gmail.com>
2025-08-27 13:25:37 -0300

    Finalizando desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,17 +15,23 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
+            // Implementado conforme instruções no Readme.md
+            if (hospedes.Count <= Suite.Capacidade)
+
+                if (true)
+                {
+                    Hospedes = hospedes;
+                }
+                else
+                {
+                    // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                    // Implementado conforme instruções no Readme.md
+                    throw new InvalidOperationException(
+                $"A quantidade de hóspedes ({hospedes.Count}) excede a capacidade da suíte ({Suite.Capacidade})."
+            );
+                }
         }
 
         public void CadastrarSuite(Suite suite)
@@ -36,23 +42,24 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // Implementado conforme instruções no Readme.md
+            return Hospedes?.Count ?? 0;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // // Implementado conforme instruções no Readme.md
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // // Implementado conforme instruções no Readme.md
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= valor * 0.10m; // Aplica um desconto de 10%
             }
+
 
             return valor;
         }
